fc25eb6b85a6660b787be54c9578128d13976ea0
Fasbeer Eskander <feskander21@outlook.com>
2017-03-19 18:01:49 +0000

    my charts

---
+++ Prototype/Fasbeer Charts/Line Graph and Table/Button1.js
@@ -0,0 +1,7 @@
+//get a reference to the element
+  var myBtn = document.getElementById('myButton');
+
+  //add event listener
+  myBtn.addEventListener('click', function(event) {
+    window.location.href='Graph and Table.html';
+  });
